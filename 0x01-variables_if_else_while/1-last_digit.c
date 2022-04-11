e3b885717923b75b2a22ce687ae986abe664a512
Michealchuks <michealchuks66@gmail.com>
2022-04-11 05:24:38 -0700

    num 1 task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success/Correct)
  */
 int main(void)
 
@@ -18,17 +18,17 @@ n = rand() - RAND_MAX / 2;
 
 if (n % 10 > 5)
 {
-printf("last digit of %d is %d and is greater than 5\n", n, n % 10);
+printf("last digit of %i is %i and is greater than 5\n", n, n % 10);
 }
 
 else if (n % 10 == 0)
 {
-printf("last digit of %d is %d and is 0\n", n, n % 10);
+printf("last digit of %i is %i and is 0\n", n, n % 10);
 }
 
 else
 {
-printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+printf("last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
 
 return (0);
